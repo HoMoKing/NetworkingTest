7bf4ed93f7fd38712b0d33638d81f6c793f58b32
AngusKing <xiaojunzhizuo@sina.com>
2016-01-08 13:38:14 +0800

    添加全局文件

---
+++ cocoapodsTest/cocoapodsTest/Other/Common.h
@@ -0,0 +1,15 @@
+//
+//  Common.h
+//  cocoapodsTest
+//
+//  Created by mac on 16/1/8.
+//  Copyright © 2016年 王永军. All rights reserved.
+//
+
+#ifndef Common_h
+#define Common_h
+
+
+#endif /* Common_h */
+
+
---
+++ cocoapodsTest/cocoapodsTest/Other/SDImport.h
@@ -0,0 +1,18 @@
+//
+//  SDImport.h
+//  cocoapodsTest
+//
+//  Created by mac on 16/1/8.
+//  Copyright © 2016年 王永军. All rights reserved.
+//
+
+#ifndef SDImport_h
+#define SDImport_h
+
+
+#endif /* SDImport_h */
+
+
+#import "AFNetworking.h"
+#import "Masonry.h"
+#import "MBProgressHUD.h"
