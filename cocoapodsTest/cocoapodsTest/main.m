e08affa83c027162b420f981a315dd1fa5f401a5
AngusKing <xiaojunzhizuo@sina.com>
2016-01-08 13:23:42 +0800

    新建空工程

---
+++ cocoapodsTest/cocoapodsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cocoapodsTest
+//
+//  Created by mac on 16/1/8.
+//  Copyright © 2016年 王永军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
